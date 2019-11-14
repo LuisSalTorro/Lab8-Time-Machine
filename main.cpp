b6bf66499e75d9ed323b855d272fead97f8e2389
EvanStJohn <46731712+EvanStJohn@users.noreply.github.com>
2019-11-13 18:26:06 -0800

    update main

--- main.cpp
+++ main.cpp
@@ -10,20 +10,14 @@ public:
         int year_in_future;
     };
     void decide_the_future();
-
 };
 
 class CPresent{
 public:
-    struct fifth_dimension{
-
-    };
     void biologicalClock(CFuture::fifth_dimension dimension);
 };
 
 class CTimeMachine{
-private:
-
 public:
     void API();
 };
@@ -33,27 +27,45 @@ void CTimeMachine::API() {
     CFuture future, *futurePointer; //the number of future objects depends on how many are alive
     presentPointer = &present;
     futurePointer = &future;
+    futurePointer->decide_the_future();
     //pass the *present into future
 
 }
 
 void CFuture::decide_the_future()
 {
-    fifth_dimension dimension;
+    fifth_dimension dimension, *dimensionPointer;
+    dimensionPointer = &dimension;
 
-    cout << "What is Luke's lifespan" << endl;
-    cin >> dimension.remaining_life_span;
-    cout << "what is the future year" << endl;
-    cin >> dimension.year_in_future;
-    cout << "Does Darth Vader kill or spare Luke (0/1)" << endl;
-    cin >> dimension.alive;
+    int input;
+    cout << "how many people are there" << endl;
+    cin >> input;
+    dimensionPointer = new fifth_dimension[input];
+
+    for (int i = 0; i < input; i++)
+    {
+        cout << "What is Luke's lifespan" << endl;
+        cin >> dimensionPointer[i].remaining_life_span;
+        cout << "what is the future year" << endl;
+        cin >> dimensionPointer[i].year_in_future;
+        cout << "Does Darth Vader kill or spare Luke (0/1)" << endl;
+        cin >> dimensionPointer[i].alive;
+    }
 }
 
 void CPresent::biologicalClock(CFuture::fifth_dimension dimension)
 {
     for (int i = 0; i < dimension.remaining_life_span; i++)
     {
-        // print
+        if(dimension.remaining_life_span > 0)
+        {
+            cout << "alive: " << dimension.name << "has "
+            << dimension.remaining_life_span << " years left" << endl;
+        }
+        else
+        {
+            cout << "darth vader decided to kill" << dimension.name << endl;
+        }
     }
 }
 
